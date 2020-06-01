c06aaa87ea1b6c3157f50103df640b02de5d0d89
marcuscrescenzo <marcus.crescenzo@gmail.com>
2020-06-01 11:08:29 -0300

    novos

---
+++ sql.php
@@ -0,0 +1,42 @@
+<?php
+
+class sql extends PDO {
+
+    private $conn;
+
+    public function __construct() {
+    	$this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "");
+    }
+
+    private function setParams($statment, $parameters = array()){
+
+    	foreach ($parameters as $key => $value) {
+
+    		$this->bindParam($key, $value);
+    	}
+    }
+
+    private function setParams($statment, $key, $value) {
+
+    	$statment->bindParam($key, $value);
+    }
+
+    public function query($rawQuery, $params = array()) {
+    	$stmt = $this->conn->prepare($rawQuery);
+
+    	$this->setParams($stmt, $params);
+
+    	return $stmt->execute;
+    		}
+
+public function select($rawQuery, $params = array()):array
+{
+
+	$stmt = $this->query($rawQuery, $params);
+	$stmt = fetchAll(PDO::FETCH_ASSOC);
+
+}
+
+}
+
+?>
